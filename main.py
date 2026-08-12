fd345a28b0fbd4f5fdfd333bfad9d75b5c1afc43
Aditi Pandey <aditip301206@gmail.com>
2026-08-13 03:06:31 +0530

    add user model, schema, and GET /users endpoint

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from sqlalchemy import text,select
 from database import get_db
 from models.company import Company
 from schemas.company import CompanyOut
+from models.user import User
+from schemas.user import UserOut
 
 app = FastAPI(title="Chaos Exchange API")
 
@@ -22,3 +24,10 @@ async def get_companies(db: AsyncSession = Depends(get_db)):
     result = await db.execute(select(Company))
     companies = result.scalars().all()
     return companies
+
+@app.get("/users",response_model=list[UserOut])
+async def get_users(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(select(User))
+    users = result.scalars().all()
+    return users
+
