2b2e73b1ebf25e7036cc03734e6880cba0544a17
Aditi Pandey <aditip301206@gmail.com>
2026-08-14 03:42:23 +0530

    split companies and users into their own routers, add GET /companies/{id}

--- main.py
+++ main.py
@@ -3,13 +3,14 @@ import asyncio
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import text,select
 from database import get_db
-from models.company import Company
-from schemas.company import CompanyOut
-from models.user import User
-from schemas.user import UserOut
+from models.company import Company #prolly unused
+from schemas.company import CompanyOut #prolly used
+from models.user import User #prolly used
+from schemas.user import UserOut #prolly used
 from routers import trading
 from routers import portfolio
 from routers import leaderboard
+from routers import companies, users
 from redis_client import redis_client
 from fastapi import WebSocket, WebSocketDisconnect
 from websocket.manager import manager
@@ -29,6 +30,8 @@ async def startup_event():
 app.include_router(trading.router)
 app.include_router(portfolio.router)
 app.include_router(leaderboard.router)
+app.include_router(companies.router)
+app.include_router(users.router)
 
 @app.get("/")
 async def root():
@@ -39,18 +42,6 @@ async def db_check(db: AsyncSession = Depends(get_db)):
     result = await db.execute(text("SELECT COUNT(*) FROM companies"))
     count = result.scalar()
     return {"companies_in_db": count}
-
-@app.get("/companies", response_model=list[CompanyOut])
-async def get_companies(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(Company))
-    companies = result.scalars().all()
-    return companies
-
-@app.get("/users",response_model=list[UserOut])
-async def get_users(db: AsyncSession = Depends(get_db)):
-    result = await db.execute(select(User))
-    users = result.scalars().all()
-    return users
 
 @app.get("/redis-check")
 async def redis_check():
